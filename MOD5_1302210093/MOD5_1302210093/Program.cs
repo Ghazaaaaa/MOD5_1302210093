9694bde006d7bceb089ac87ea565b9079d24d31b
Ghazaaaaa <gymnastiarghaza@gmail.com>
2023-03-19 23:34:02 +0700

    menambahkan class DataGeneric

--- MOD5_1302210093/MOD5_1302210093/Program.cs
+++ MOD5_1302210093/MOD5_1302210093/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataGeneric dataGeneric = new DataGeneric("1302210093");
+        dataGeneric.printData();
+    }
+}
+
+internal class DataGeneric
+{
+    string t;
+    public DataGeneric(string t)
+    {
+        this.t = t;
+    }
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah : " + t);
+    }
+}
